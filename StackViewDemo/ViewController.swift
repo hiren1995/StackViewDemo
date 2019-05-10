febb1c9ec2e5d6ae8bb34ace91f02b5b70dd1797
Hiren Kadam <hiren.kadam@logicalwingsindia.com>
2019-05-10 14:24:31 +0530

    10/5/19-2:25pm-Initial COmmit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by LogicalWings Mac on 08/08/18.
+//  Copyright © 2018 LogicalWings Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
